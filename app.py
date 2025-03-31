c7836176eb5db578567893589fbde28fcb54fd39
Rubén Lázaro <phone75firefox@gmail.com>
2025-03-31 17:16:34 +0200

    plantilla flask

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from flask import Flask, render_template
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 from dotenv import load_dotenv
@@ -6,20 +7,30 @@ from dotenv import load_dotenv
 # Cargar variables del archivo .env
 load_dotenv()
 
+app = Flask(__name__)
+
 # Obtener credenciales desde el .env
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 SECURE_CONNECT_BUNDLE = os.getenv("SECURE_CONNECT_BUNDLE")
 
-# Configurar la conexión a Cassandra
+# Conectar a Cassandra
 cloud_config = {"secure_connect_bundle": SECURE_CONNECT_BUNDLE}
 auth_provider = PlainTextAuthProvider(CLIENT_ID, CLIENT_SECRET)
 cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
 session = cluster.connect()
 
-# Probar conexión
-row = session.execute("SELECT release_version FROM system.local").one()
-if row:
-    print(f"Conectado a Cassandra, versión: {row[0]}")
-else:
-    print("Error en la conexión a Cassandra")
+
+session.set_keyspace("prueba")
+
+
+@app.route("/")
+def index():
+    # Obtener los clientes de la base de datos
+    clientes = session.execute("SELECT * FROM cliente").all()
+
+    return render_template("index.html", clientes=clientes)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
